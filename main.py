780a21d744be9d5bbf0a5c966b8cbc91dc15b78d
SL.Leo <1324901134@qq.com>
2016-10-09 23:15:01 +0800

    first

---
+++ main.py
@@ -0,0 +1,37 @@
+from urllib.request import *
+import urllib.parse
+import pickle
+data={
+	"username":"1341101168",
+	"ac_id":"1",
+	"save_me":"1",
+	"ajax":"1"}
+url="http://172.18.1.95:801/srun_portal_pc.php?ac_id=1&"
+def login(url,data):
+          data=urllib.parse.urlencode(data).encode("utf-8")
+
+
+          get=urlopen(url,data)
+          print(get.read().decode("utf-8"))
+def logout(url,data):
+          data=urllib.parse.urlencode(data).encode("utf-8")
+          
+          get=urlopen(url,data)
+          print(get.read().decode("utf-8"))
+
+if __name__=="__main__":
+          num=int(input("请选择功能(1 校园网登录。2 校园网注销）:"))
+          
+          data["password"]=str(input("请输入密码："))
+          if num==1:
+                    data["action"]="login"
+                    login(url,data)
+          if num==2:
+                    data["action"]="logout"
+                    logout(url,data)
+          elif num==0:
+                    exit()
+
+
+
+          
